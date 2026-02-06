3b5b7b7b5f2ce04f68032665463923348176528f
aangisiroya <aangisiroya91@gmail.com>
2026-02-06 20:55:54 +0530

    FoldersList

---
+++ app/src/main/java/com/example/patientapp/FolderModel.java
@@ -0,0 +1,16 @@
+package com.example.patientapp;
+
+public class FolderModel {
+
+    public String folderId;
+    public String folderName;
+    public String date;
+    public String hospital;
+    public String category;
+    public String description;
+    public long createdAt;
+
+    public FolderModel() {
+        // required for Firebase
+    }
+}
